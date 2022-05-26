61ee7bdc9803673c13866a46f5543e51964708a7
Mukarrom <mukarrom21@yahoo.com>
2022-05-27 00:31:24 +0600

    running problem solved

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+// require
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+// express
 const app = express();
+// port
 const port = process.env.PORT || 5000;
-
 // middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('assignment 12 server is running')
-})
+// Check in the browser http://localhost:5000
+app.get("/", (req, res) => {
+  res.send("assignment 12 running");
+});
 
-app.listen(()=>{
-    console.log('mmh12', port);
-})
+app.listen(port, () => {
+  console.log("mmh12", port);
+});
