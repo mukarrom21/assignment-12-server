941c1b7286e53a0e4b87649de93b57e39d899014
mukarrom21 <mukarrom21@gmail.com>
2022-05-28 13:47:43 +0600

    get api created

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // require
 const express = require("express");
 const cors = require("cors");
+// const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 // express
 const app = express();
@@ -9,6 +11,33 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+// connect with mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fhqdpjy.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// ----------------------------------------
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("assignment_12").collection("products");
+    // get api
+    app.get("/products", async (req, res) => {
+      const products = await productCollection.find().toArray();
+      res.send(products);
+    });
+
+    
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 // Check in the browser http://localhost:5000
 app.get("/", (req, res) => {
